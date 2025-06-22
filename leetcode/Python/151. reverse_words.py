1a0a2208835d5e7f27eca916fd70d0464ad0df83
Sushravya-Mohan <67335303+Sushravya-Mohan@users.noreply.github.com>
2025-06-22 08:39:10 +0000

    fresh start

--- leetcode/Python/151. reverse_words.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        str_lst = s.split()
-        reverse_str = " ".join(str_lst[::-1])
-        return reverse_str
--- leetcode/Python/268. missing_number.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        missing_number = sum(range(len(nums)+1)) - sum(nums)
-        return missing_number
